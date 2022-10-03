f2390a3abd430d14a4884b29da192dc70dafdef8
Ridwan Arifandi <ridwan@99.co>
2022-10-03 18:53:45 +0700

    feat(remove): able to remove card

--- components/TimeDiff/index.tsx
+++ components/TimeDiff/index.tsx
@@ -1,13 +1,16 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, useContext } from "react"
+import AppContext from "../../context/AppContext"
 
 interface TimeDiffInterface {
   unixtime: number
   timezone: string
   abbreviation: string
+  title: string
 }
 
 const TimeDiffComponent: FunctionComponent<TimeDiffInterface> = (props) => {
-  const { unixtime, timezone, abbreviation } = props
+  const context = useContext(AppContext)
+  const { unixtime, timezone, abbreviation, title } = props
 
   const hour = new Date(unixtime).toLocaleString("en-US", {
     timeZone: timezone,
@@ -17,10 +20,16 @@ const TimeDiffComponent: FunctionComponent<TimeDiffInterface> = (props) => {
   })
 
   return (
-    <div className="text-center ml-5">
-      <h3>
-        {hour} {abbreviation}
-      </h3>
+    <div className="relative border border-black text-center py-4 px-6 mx-2">
+      <span
+        className="absolute cursor-pointer top-0 right-1"
+        onClick={() => context.removeTimeZoneCard(timezone)}
+      >
+        x
+      </span>
+      <h1 className="leading-6 text-xl">{title}</h1>
+      <h3 className="text-4xl">{hour}</h3>
+      <h4>{abbreviation}</h4>
     </div>
   )
 }
